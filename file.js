838570d3558004b1ef502d6014be0db3e09ea2be
will-a-creator <68308538+will-a-creator@users.noreply.github.com>
2021-05-17 18:25:22 -0400

    Create file.js

---
+++ file.js
@@ -0,0 +1,12 @@
+//THEODINPROJECT
+const palindrome = function(string){
+     newString = string.toLowerCase();
+
+     return (newString.split("").reverse().join("") == newString);
+};
+
+
+
+
+
+module.exports = palindromes
